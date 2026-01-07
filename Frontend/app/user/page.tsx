d683d4ebbdb1b4f3ed2c88194b174f6a2997ac44
deepak singla <singladeepak519@gmail.com>
2026-01-07 14:34:54 +0530

    frountend changes

--- Frontend/app/user/page.tsx
+++ Frontend/app/user/page.tsx
@@ -1,11 +1,18 @@
 
 import Sidebar from "./_components/sidebar_u"
-
+import Banner from "./_components/Banner"
 export default function page() {
   return (
-    <div>
-<Sidebar></Sidebar>
+    <div className="flex min-h-screen">
+    {/* Sidebar */}
+    <Sidebar />
+    
 
-    </div>
+    {/* Main Content */}
+    <main className="flex-1 ">
+      <Banner></Banner>
+   \
+    </main>
+  </div>
   )
 }
